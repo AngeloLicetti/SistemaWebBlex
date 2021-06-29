8aac83913500a21d8909a6f41bbc719b243cf4bb
Angelord Licetti <angelo.licetti.00@gmail.com>
2021-06-29 18:35:15 -0500

    Agregar archivos de proyecto.

---
+++ BlexDigital/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace BlexDigital.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BlexDigital.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(BlexDigital.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
